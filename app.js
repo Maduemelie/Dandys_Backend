c431dfb0ea27913d31160c76f911d4ebeec39ffe
Maduemelie <maduemelie@gmail.com>
2024-02-28 07:59:22 +0100

    origin

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://dandys-auth-app.onrender.com'
+  );
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
