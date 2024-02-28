cbb0d455b6d76a3944f93ee7b99e04b72d2118ec
Maduemelie <maduemelie@gmail.com>
2024-02-28 09:35:50 +0100

    localhost

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://dandys-auth-app.onrender.com'
-  );
+  const allowedOrigins = [
+    'https://dandys-auth-app.onrender.com',
+    'http://localhost:3000',
+    // Add more origins as needed
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
@@ -19,11 +27,15 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   );
-  next();
+
+  // Check if this is a preflight request
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200); // Respond with 200 OK for preflight requests
+  } else {
+    next(); // Move to the next middleware
+  }
 });
-app.options('*', (req, res) => {
-  res.sendStatus(200);
-});
+
 app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
